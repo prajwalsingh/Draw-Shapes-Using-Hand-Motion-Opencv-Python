2fc1f971eea9b2baa666b69ed66bba8cbf1a5d87
Prajwal Singh <prajwalrishabh@gmail.com>
2017-06-27 02:28:20 +0530

    Update GesturePlot.py

--- GesturePlot.py
+++ GesturePlot.py
@@ -5,16 +5,18 @@ cap = cv2.VideoCapture(0)
 
 points = []
 
+flag = 0
+
 while(1):
 
 	_,frame = cap.read()
 
-	filterFrame = cv2.GaussianBlur(frame,(25,25),15)
+	filterFrame = cv2.GaussianBlur(frame,(35,35),25)
 
 	hsvFrame = cv2.cvtColor(filterFrame,cv2.COLOR_BGR2HSV)
 
-	lower_bound = numpy.array([0,110,110])
-	upper_bound = numpy.array([130,255,255])
+	lower_bound = numpy.array([30,100,100])
+	upper_bound = numpy.array([80,255,255])
 
 
 	threshImg = cv2.inRange(hsvFrame,lower_bound,upper_bound)	
@@ -29,25 +31,35 @@ while(1):
 	
 	X = 0
 	Y = 0
-	
-	for item in contours:
-		for i in item :
-			X += i[0][0]
-			Y += i[0][1]
-			c += 1
 
-	try:			
-		points.append([int(X/c),int(Y/c)])		
-	except:
-		pass
+	key = cv2.waitKey(1)
+
+	if flag==1:
+		for item in contours:
+			for i in item :
+				X += i[0][0]
+				Y += i[0][1]
+				c += 1
+
+		try:			
+			points.append([int(X/c),int(Y/c)])		
+		except:
+			pass
+
+
+	if (key & 0xFF == ord('s')) and flag == 0:
+		flag = 1
+
+	elif key & 0xFF == ord('s') and flag == 1 :
+		flag = 0
 
 
 	for p in points:
-		cv2.circle(finalImg,tuple(p),20,(0,0,255),-1)
+			cv2.circle(finalImg,tuple(p),20,(0,0,255),-1)	
 
 	cv2.imshow('HSV',finalImg)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	if key & 0xFF == ord('q'):
 		break
 
 cv2.destroyAllWindows()
